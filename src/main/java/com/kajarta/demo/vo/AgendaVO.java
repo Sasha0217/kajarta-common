f34ab751e368405317649a98f3188071fe5febb9
Enzo <d900124@gmail.com>
2024-07-11 08:37:46 +0800

    AgendaVO

---
+++ src/main/java/com/kajarta/demo/vo/AgendaVO.java
@@ -0,0 +1,41 @@
+package com.kajarta.demo.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+import com.kajarta.demo.model.Employee;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AgendaVO {
+    private Integer id;
+
+    @Schema(description = "排程員工")
+    private Employee employee;
+
+    @Schema(description = "事由")
+    private String businessPurpose;
+
+    @Schema(description = "排程開始時間")
+    private Date unavailableTimeStr;
+
+    @Schema(description = "排程結束時間")
+    private Date unavailableTimeEnd;
+
+    @Schema(description = "排程分類")
+    private Integer unavailableStatus;
+
+    @Schema(description = "創建時間")
+    private Date createTime;
+
+    @Schema(description = "更新時間")
+    private Date updateTime;
+
+}
