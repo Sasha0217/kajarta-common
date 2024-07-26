84fa522909d19a03d3914c1f923140ef6e35e7e2
stanley1210 <stbs1210@gmail.com>
2024-07-26 10:58:00 +0800

    123

--- src/main/java/com/kajarta/demo/enums/ViewTimeSectionEnum.java
+++ src/main/java/com/kajarta/demo/enums/ViewTimeSectionEnum.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 @Getter
 public enum ViewTimeSectionEnum {
 
-    MORNING_SESSION(1, "10:00:00-12:00:00"),
-    AFTERNOON_SESSION_1(2, "13:00:00-15:00:00"),
-    AFTERNOON_SESSION_2(3, "15:00:00-17:00:00"),
-    EVENING_SESSION(4, "17:00:00-19:00:00");
+    MORNING_SESSION(1, "10:01:00-11:59:00"),
+    AFTERNOON_SESSION_1(2, "13:01:00-14:59:00"),
+    AFTERNOON_SESSION_2(3, "15:01:00-16:59:00"),
+    EVENING_SESSION(4, "17:01:00-18:59:00");
 
     private final int code;
     private final String timeRange;
